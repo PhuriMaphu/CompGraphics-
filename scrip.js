af6502e9a8546619376663310774ae4df5248ff6
PhuriMaphu <phurimaphu@gmail.com>
2025-07-01 20:45:55 +0700

    เี่ยม

--- scrip.js
+++ scrip.js
@@ -3,9 +3,10 @@ console.log("Hello World!!");
 const canvas = document.querySelector("#myCanvas"); // ใช้ ID ของ canvas ที่ต้องการเข้าถึง
 const ctx = canvas.getContext("2d"); // ใช้ 2D context สำหรับวาดกราฟิก
 const img = new Image(); // สร้างออบเจ็กต์ภาพ
-	img.src = "./Screenshot 2025-05-25 140826.png"; // กำหนด URL ของภาพ
+	img.src = "./31dke4F+cTL.jpg"; // กำหนด URL ของภาพ
 	img.onload = function() { // เมื่อภาพโหลดเสร็จ
-		ctx.drawImage(img, 400, 50, 200, 200); // วาดภาพบน canvas
+		ctx.drawImage(img, 400, 50, 400, 200); // วาดภาพบน canvas
 	};
-
+ctx.fillStyle = "rgb(45, 138, 8)";
+ctx.fillRect(50, 50, 50, 50);
 });
