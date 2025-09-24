ee6d36dadbc6ccb47d25b66ac712a7b0480b6a6d
PhuriMaphu <phurimaphu@gmail.com>
2025-09-24 12:14:09 +0700

    exam

--- scrip.js
+++ scrip.js
@@ -11,92 +11,14 @@ const img = new Image(); // สร้างออบเจ็กต์ภาพ
 
 
 
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.fillStyle = "rgb(234, 249, 234)"; //สี่เหลี่ยม
-ctx.strokeRect(300, 200, 150, 150);
 
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.fillStyle = "rgb(234, 249, 234)"; //สี่เหลี่ยม
-ctx.fillRect(0, 0, 150, 150);
-
-
-ctx.strokeStyle = "red";
-	// วาดเส้น
-	ctx.beginPath(); // กำหนดให้เริ่มวาดเส้น
-	ctx.arc(500,400, 50, 3, 10); // กำหนดลักษณะเส้นที่วาด arc คือเส้นโค้ง
-	//ctx.closePath(); // คือทำให้มีการลงเส้นกลับมาที่จุดเริ่มต้น
-	ctx.stroke(); // วาดเส้นตามที่กำหนด
-
-    ctx.font = "20px Tahoma"; //ข้อความ
-	ctx.fillStyle = "black";
-	ctx.fillText("Hello",500,400);
-
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(100, 80); // จุดเริ่มต้นของเส้น
-	ctx.lineTo(500, 400); // จุดต่อไปของเส้น
-	ctx.lineTo(250, 450); // จุดสิ้นสุดของเส้น
-	ctx.lineTo(100, 80); // จุดสิ้นสุดของเส้น
-	ctx.lineCap = "round"; // กำหนดรูปแบบปลายเส้น
-	ctx.strokeStyle = "purple"; // กำหนดสีเส้น
-	ctx.lineWidth = 8; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้นตรง
-
-    ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(0, 0); // จุดเริ่มต้นของเส้น
-	ctx.lineTo(2000, 800); // จุดต่อไปของเส้น
-	ctx.lineCap = "round"; // กำหนดรูปแบบปลายเส้น
-	ctx.strokeStyle = "orange"; // กำหนดสีเส้น
-	ctx.lineWidth = 8; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้นตรง
-
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(50, 500); // จุดเริ่มต้นของ curve
-	ctx.quadraticCurveTo(150, 400, 250, 500); // วาด curve แบบ quadratic
-	ctx.strokeStyle = "pink"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve
-
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(300, 500); // จุดเริ่มต้นของ curve
-	ctx.bezierCurveTo(400, 600, 500, 400, 600, 500); // วาด curve แบบ bezier
-	ctx.strokeStyle = "red"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve
-	
-/*ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(1200, 600); // จุดเริ่มต้นของ curve
-	ctx.quadraticCurveTo(1000, 500, 1200, 700); // วาด curve แบบ quadratic
-	ctx.strokeStyle = "red"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve
-
-ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(1200, 600); // จุดริ่มต้นของ curve
-	ctx.quadraticCurveTo(1340, 500, 1200, 700); // วาด curve แบบ quadratic
-	ctx.strokeStyle = "red"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve*/
-    ctx.save(); // บันทึกสถานะปัจจุบันของ context
-	ctx.translate(1000, 400); // ย้ายจุดเริ่มต้นของการวาดไปที่ (400, 300)
-	ctx.rotate(0); // หมุนกราฟิก 0 องศา (0 เรเดียน)
-	ctx.scale(1, 1); // ขนาดกราฟิกเท่าเดิม
-
-	ctx.fillStyle = "rgb(255, 0, 0)"; // กำหนดสีเขียวสำหรับการเติมสี
-	ctx.strokeStyle = "rgb(0, 0, 0)"; // กำหนดสีเส้นขอบเป็นสีดำ
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(0, -50); // ย้ายจุดเริ่มต้นของเส้นทางไปที่ (-25, -25)
-	ctx.bezierCurveTo(70, -90, 90, 20, 0, 50); // วาดเส้นโค้ง Bezier
-	ctx.bezierCurveTo(-90, 20, -70, -90, 0, -50); // วาดเส้นโค้ง Bezier
-	ctx.fill(); // เติมสีภายในเส้นทางที่กำหนด
-	ctx.stroke(); // วาดเส้นตามเส้นทางที่กำหนด
-    ctx.restore(); 
 
 	let angle = 0; // มุมเริ่มต้นสำหรับการหมุน
 	let lastTime = 0; // ตัวแปรสำหรับเก็บเวลาของเฟรมก่อนหน้า
 	let fps; // ตัวแปรสำหรับเก็บค่าเฟรมต่อวินาที (FPS)
-
+	let xx = 0;
+	let yy = 400;
+let direction = 1;
 	// ฟังก์ชัน animate จะถูกเรียกใช้เพื่อวาดกราฟิกใหม่ในแต่ละเฟรม
 	function animate() {
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // ล้าง canvas
@@ -115,14 +37,20 @@ ctx.beginPath(); // เริ่มเส้นทางใหม่
 		ctx.restore(); // คืนสถานะ context กลับไปยังสถานะที่บันทึกไว้ก่อนหน้านี้
 
 		ctx.save(); // บันทึกสถานะปัจจุบันของ context
-		ctx.translate(400, 300); // ย้ายจุดเริ่มต้นของการวาดไปที่ (400, 300)
+		ctx.translate(xx, yy); // ย้ายจุดเริ่มต้นของการวาดไปที่ (400, 300)
 		ctx.rotate(angle); // หมุนกราฟิกตามมุมที่กำหนด 
 		ctx.scale(1, 1); // ขนาดกราฟิกเท่าเดิม
-		// วาดหัวใจ
-		ctx.drawImage(img, 100, 100, 500, 281);
+		ctx.drawImage(img, -250, -140.5, 500, 281);
 		ctx.restore(); // คืนสถานะ context กลับไปยังสถานะที่บันทึกไว้ก่อนหน้านี้
 
-		angle += 0.01; // เพิ่มมุมสำหรับการหมุนในครั้งถัดไป
+
+    	if (xx >= canvas.width) direction = -1;  
+    	else if (xx <= 0) direction = 1;
+    	xx += 10 * direction;
+		
+	
+
+		angle += 0.02; // เพิ่มมุมเพื่อให้เกิดการหมุนในเฟรมถัดไป
 		
 		requestAnimationFrame(animate); // เรียกใช้ฟังก์ชัน animate อีกครั้งเพื่อสร้างการเคลื่อนไหวต่อเนื่อง
 	}
