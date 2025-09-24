f0fe4342fb6810a765b55a227b8094d23a44d136
PhuriMaphu <phurimaphu@gmail.com>
2025-09-24 13:06:01 +0700

    realexam

--- ddp.js
+++ ddp.js
@@ -3,88 +3,20 @@ console.log("Hello World!!");
 const canvas = document.querySelector("#myCanvas"); // ใช้ ID ของ canvas ที่ต้องการเข้าถึง
 const ctx = canvas.getContext("2d"); // ใช้ 2D context สำหรับวาดกราฟิก
 
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.fillStyle = "rgb(234, 249, 234)"; //สี่เหลี่ยม
-ctx.strokeRect(300, 200, 150, 150);
 
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.fillStyle = "rgb(234, 249, 234)"; //สี่เหลี่ยม
-ctx.fillRect(0, 0, 150, 150);
-
-
-ctx.strokeStyle = "red";
-	// วาดเส้น
-	ctx.beginPath(); // กำหนดให้เริ่มวาดเส้น
-	ctx.arc(500,400, 50, 3, 10); // กำหนดลักษณะเส้นที่วาด arc คือเส้นโค้ง
-	//ctx.closePath(); // คือทำให้มีการลงเส้นกลับมาที่จุดเริ่มต้น
-	ctx.stroke(); // วาดเส้นตามที่กำหนด
-
-    ctx.font = "20px Tahoma"; //ข้อความ
-	ctx.fillStyle = "black";
-	ctx.fillText("Hello",500,400);
-
-    ctx.fillStyle = "#00044ff";
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(100, 80); // จุดเริ่มต้นของเส้น
-	ctx.lineTo(500, 400); // จุดต่อไปของเส้น
-	ctx.lineTo(250, 450); // จุดสิ้นสุดของเส้น
-	ctx.lineTo(100, 80); // จุดสิ้นสุดของเส้น
-	ctx.lineCap = "round"; // กำหนดรูปแบบปลายเส้น
-	ctx.strokeStyle = "ff0000"; // กำหนดสีเส้น
-	ctx.lineWidth = 8; // กำหนดความหนาของเส้น
-    ctx.fill(); 
-	ctx.stroke(); // วาดเส้นตรง
-
-    ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(0, 0); // จุดเริ่มต้นของเส้น
-	ctx.lineTo(2000, 800); // จุดต่อไปของเส้น
-	ctx.lineCap = "round"; // กำหนดรูปแบบปลายเส้น
-	ctx.strokeStyle = "orange"; // กำหนดสีเส้น
-	ctx.lineWidth = 8; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้นตรง
-
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(50, 500); // จุดเริ่มต้นของ curve
-	ctx.quadraticCurveTo(150, 400, 250, 500); // วาด curve แบบ quadratic
-	ctx.strokeStyle = "pink"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve
-
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(300, 500); // จุดเริ่มต้นของ curve
-	ctx.bezierCurveTo(400, 600, 500, 400, 600, 500); // วาด curve แบบ bezier
-	ctx.strokeStyle = "red"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve
+	ctx.fillStyle = "blue"; // กำหนดสีเติม
+	ctx.fillRect(50, 50, 100, 100); // วาดสี่เหลี่ยมผืนผ้k
+	ctx.strokeStyle = "#ff0000"; // กำหนดสีเส้นขอบ
+	ctx.lineWidth = 5; // กำหนดความหนาของเส้น
 	
-/*ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(1200, 600); // จุดเริ่มต้นของ curve
-	ctx.quadraticCurveTo(1000, 500, 1200, 700); // วาด curve แบบ quadratic
-	ctx.strokeStyle = "red"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve
 
-ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(1200, 600); // จุดริ่มต้นของ curve
-	ctx.quadraticCurveTo(1340, 500, 1200, 700); // วาด curve แบบ quadratic
-	ctx.strokeStyle = "red"; // กำหนดสีเส้น
-	ctx.lineWidth = 4; // กำหนดความหนาของเส้น
-	ctx.stroke(); // วาดเส้น curve*/
-    ctx.save(); // บันทึกสถานะปัจจุบันของ context
-	ctx.translate(1000, 400); // ย้ายจุดเริ่มต้นของการวาดไปที่ (400, 300)
-	ctx.rotate(0); // หมุนกราฟิก 0 องศา (0 เรเดียน)
-	ctx.scale(1, 1); // ขนาดกราฟิกเท่าเดิม
+	ctx.beginPath();
+	ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+	ctx.fillStyle = "red";
+	ctx.fill();
+	ctx.lineWidth = 4;
+	ctx.strokeStyle = "#ff0000";
+	ctx.stroke();
 
-	ctx.fillStyle = "rgb(255, 0, 0)"; // กำหนดสีเขียวสำหรับการเติมสี
-	ctx.strokeStyle = "rgb(0, 0, 0)"; // กำหนดสีเส้นขอบเป็นสีดำ
-	ctx.beginPath(); // เริ่มเส้นทางใหม่
-	ctx.moveTo(0, -50); // ย้ายจุดเริ่มต้นของเส้นทางไปที่ (-25, -25)
-	ctx.bezierCurveTo(70, -90, 90, 20, 0, 50); // วาดเส้นโค้ง Bezier
-	ctx.bezierCurveTo(-90, 20, -70, -90, 0, -50); // วาดเส้นโค้ง Bezier
-	ctx.fill(); // เติมสีภายในเส้นทางที่กำหนด
-	ctx.stroke(); // วาดเส้นตามเส้นทางที่กำหนด
-    ctx.restore(); 
 
 });
--- scrip.js
+++ scrip.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector("#myCanvas"); // ใช้ ID ของ ca
 const ctx = canvas.getContext("2d"); // ใช้ 2D context สำหรับวาดกราฟิก
 
 const img = new Image(); // สร้างออบเจ็กต์ภาพ
-	img.src = "./31dke4F+cTL.jpg"; // กำหนด URL ของภาพ
+	img.src = "./gettyimages-180680638-676f621f720bc.avif"; // กำหนด URL ของภาพ
 	/*img.onload = function() { // เมื่อภาพโหลดเสร็จ
 		ctx.drawImage(img, 1200, 100, 500, 281); // วาดภาพบน canvas
 	};*/
@@ -16,15 +16,13 @@ const img = new Image(); // สร้างออบเจ็กต์ภาพ
 	let angle = 0; // มุมเริ่มต้นสำหรับการหมุน
 	let lastTime = 0; // ตัวแปรสำหรับเก็บเวลาของเฟรมก่อนหน้า
 	let fps; // ตัวแปรสำหรับเก็บค่าเฟรมต่อวินาที (FPS)
-	let xx = 0;
-	let yy = 400;
 let direction = 1;
 	// ฟังก์ชัน animate จะถูกเรียกใช้เพื่อวาดกราฟิกใหม่ในแต่ละเฟรม
 	function animate() {
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // ล้าง canvas
 		// คำนวณค่าเฟรมต่อวินาที (FPS)
 		const currentTime = performance.now(); // รับเวลาปัจจุบัน
-		const deltaTime = currentTime - lastTime; // คำนวณเวลาที่ผ่านไปตั้งแต่เฟรมก่อนหน้า
+		const deltaTime = (currentTime - lastTime) / 1000; // คำนวณเวลาที่ผ่านไปตั้งแต่เฟรมก่อนหน้า
 		lastTime = currentTime; // อัปเดตเวลาของเฟรมก่อนหน้า
 		fps = 1000 / deltaTime; // คำนวณค่าเฟรมต่อวินาที (FPS) 1000 มิลลิวินาทีหารด้วยเวลาที่ผ่านไปในมิลลิวินาที
 		
@@ -37,20 +35,15 @@ let direction = 1;
 		ctx.restore(); // คืนสถานะ context กลับไปยังสถานะที่บันทึกไว้ก่อนหน้านี้
 
 		ctx.save(); // บันทึกสถานะปัจจุบันของ context
-		ctx.translate(xx, yy); // ย้ายจุดเริ่มต้นของการวาดไปที่ (400, 300)
+		ctx.translate(1000, 400); // ย้ายจุดเริ่มต้นของการวาดไปที่ (400, 300)
 		ctx.rotate(angle); // หมุนกราฟิกตามมุมที่กำหนด 
 		ctx.scale(1, 1); // ขนาดกราฟิกเท่าเดิม
-		ctx.drawImage(img, -250, -140.5, 500, 281);
+		ctx.drawImage(img,-1000,-400,2000, 800);
 		ctx.restore(); // คืนสถานะ context กลับไปยังสถานะที่บันทึกไว้ก่อนหน้านี้
-
-
-    	if (xx >= canvas.width) direction = -1;  
-    	else if (xx <= 0) direction = 1;
-    	xx += 10 * direction;
 		
 	
 
-		angle += 0.02; // เพิ่มมุมเพื่อให้เกิดการหมุนในเฟรมถัดไป
+		angle += (Math.PI / -2) * deltaTime; // เพิ่มมุมเพื่อให้เกิดการหมุนในเฟรมถัดไป
 		
 		requestAnimationFrame(animate); // เรียกใช้ฟังก์ชัน animate อีกครั้งเพื่อสร้างการเคลื่อนไหวต่อเนื่อง
 	}
